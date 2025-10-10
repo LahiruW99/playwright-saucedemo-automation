fe491a5e075aa9dc27ad08bed73853917b6e3f27
LahiruW99 <lahiruwijesooriya99@gmail.com>
2025-10-10 18:49:56 +0530

    test: Login with Invalid Username and Click Login without username and password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -19,4 +19,22 @@ test.describe("Login Functionality", ()=>{
         await loginpage.login("standard_user", "sauce")
         await expect(page.getByText("Epic sadface: Username and password do not match any user in this service")).toBeVisible();  
     });
+
+    test("Login with Invalid Username", async({page})=>{
+        const loginpage = new LoginPage(page);
+
+        await loginpage.navigateToLoginPage();
+        await expect(page).toHaveTitle("Swag Labs");
+        await loginpage.login("standard", "secret_sauce")
+        await expect(page.getByText("Epic sadface: Username and password do not match any user in this service")).toBeVisible();  
+    });
+
+    test("Click Login without username and password", async({page})=>{
+        const loginpage = new LoginPage(page);
+
+        await loginpage.navigateToLoginPage();
+        await expect(page).toHaveTitle("Swag Labs");
+        await loginpage.login("", "")
+        await expect(page.getByText("Epic sadface: Username is required")).toBeVisible();  
+    });
 });
