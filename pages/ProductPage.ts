932407cbbce97aa95aa0502075fb88f153da50a9
LahiruW99 <lahiruwijesooriya99@gmail.com>
2025-10-30 16:44:09 +0530

    test: add tests and POM for Remove products from cart

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -3,13 +3,19 @@ import {Page, Locator} from '@playwright/test';
 export class ProductPage{
     private page: Page;
     readonly addToCartBackpackButton: Locator;
+    readonly removeBackpackFromCartButton: Locator;
 
     constructor(page:Page){
         this.page = page;
         this.addToCartBackpackButton = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')
+        this.removeBackpackFromCartButton = page.locator('[data-test="remove-sauce-labs-backpack"]')
     }
 
     async addBackpackToCart(){
         await this.addToCartBackpackButton.click();
+    }
+
+    async removeBackpackFromCart(){
+        await this.removeBackpackFromCartButton.click();
     }
 }
--- tests/productsPage.spec.ts
+++ tests/productsPage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect} from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { ProductPage } from '../pages/ProductPage';
 
-test.describe("Add Products to cart", ()=>{
+test.describe("Add Products to cart", () =>{
     test("Add Backpack to Cart", async ({page})=>{
         const loginpage = new LoginPage(page);
         await loginpage.navigateToLoginPage();
@@ -11,5 +11,19 @@ test.describe("Add Products to cart", ()=>{
 
         const productPage = new ProductPage(page);
         await productPage.addBackpackToCart();
+    });
+});
+
+test.describe("Remove Products from cart", () =>{
+    test("Remove Backpack from Cart", async ({page})=>{
+        const loginpage = new LoginPage(page);
+        await loginpage.navigateToLoginPage();
+        await loginpage.login("standard_user", "secret_sauce")
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
+
+        const productPage = new ProductPage(page);
+        await productPage.addBackpackToCart();
+        await page.waitForTimeout(5000);
+        await productPage.removeBackpackFromCart();
     });
 });
